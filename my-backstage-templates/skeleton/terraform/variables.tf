0008a9f6256d30de6fa7c9d3144d92cf808d3805
onur kaya <o.kaya@fugro.com>
2026-03-30 13:34:32 +0200

    test template

---
+++ my-backstage-templates/skeleton/terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "bucket_name" {
+  type = string
+}
+
+variable "owner" {
+  type = string
+}
+
+variable "env" {
+  type = string
+}
+
+variable "region" {
+  type    = string
+  default = "eu-west-1"
+}
